f2ffc79c8effa7d5d7bf22199f7c7112a6b713ef
lior gishry <liorgish@gmail.com>
2018-04-27 12:18:06 +0300

    more

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Recipe} from "../../models/ingredient";
 
 /**
  * Generated class for the EditRecipePage page.
@@ -13,13 +15,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-edit-recipe',
   templateUrl: 'edit-recipe.html',
 })
-export class EditRecipePage {
+export class EditRecipePage implements OnInit {
+  options = ['easy', 'medium', 'hard'];
+  private isNew: boolean;
+  private formGroup: FormGroup;
+  private recipe: Recipe;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private  fb: FormBuilder) {
+
+  }
+
+  get status(): string {
+    return this.isNew ? 'add' : 'edit';
+  }
+
+  ngOnInit(): void {
+    this.recipe = this.navParams.get('recipe') as Recipe;
+    this.isNew = this.recipe === undefined;
+    this.createForm();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditRecipePage');
+  }
+
+  onSubmit(){
+    console.log('this.formGroup ',this.formGroup)
+  }
+
+  private createForm() {
+    this.formGroup = this.fb.group({
+      name: [this.isNew ? '': this.recipe.name,[Validators.required]],
+      description: [this.isNew ? '': this.recipe.description,Validators.required],
+      difficulty: [this.isNew ? 'easy': this.recipe.difficulty,Validators.required],
+    })
   }
 
 }
--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -24,6 +24,6 @@ export class RecipesPage {
 
   onNewRecipe() {
 
-    this.navCtrl.push('RecipePage');
+    this.navCtrl.push('EditRecipePage');
   }
 }
